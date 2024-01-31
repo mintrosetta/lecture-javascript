c9cc6b09a75e4038a1fa4b27f043b84162267bf8
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-31 08:43:15 +0700

    lecture - math object

---
+++ 7_math.js
@@ -0,0 +1,14 @@
+/*
+    Math
+        เป็น object ที่ใช้ในการดำเนินการทางคณิตศาสตร์ขึ้นพื้นฐาน
+*/
+
+console.log(Math.round(3.14)); // ปัดเศษ ถ้าคำกว่า .50 จะปัดลง ถ้ามากกว่าหรือเท่ากับจะปัดขึ้น
+console.log(Math.floor(3.99)); // ปัดเศษลงเสมอ
+console.log(Math.ceil(3.01)); // ปัดขึ้นเสมอ
+console.log(Math.pow(3, 2)); // ยกกำลัง
+console.log(Math.sqrt(3.41)) // หาแสควรูท
+console.log(Math.abs(-3)) // absolute value เปลี่ยนลบให้กลายเป็นบวก
+console.log(Math.max(4, 5, 3)); // หาค่ามากที่สุดใน array
+console.log(Math.min(4, 5, 3)); // หาค่าน้อยที่สุดใน array
+console.log(Math.PI); // ดึงค่า PT
