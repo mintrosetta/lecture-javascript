74c6da17f4e3cfff0ab91a7b2a2befeef8b71176
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-01 01:33:30 +0700

    lecture - not

---
+++ 15_not_logical.js
@@ -0,0 +1,15 @@
+/* 
+    Not
+        ใช้สำหรับกลับด้านค่าจริงเท็ต
+        เช่น
+            let x = true;
+            console.log(!x); // result is false
+*/
+
+let temp = -15;
+
+if (!(temp > 0)) {
+    console.log("It's warm outside");
+} else {
+    console.log("It's cool outside");
+}
