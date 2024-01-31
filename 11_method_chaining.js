13ad2ddbee53008a9ca4a0d10ebc7aca98c55559
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-31 11:26:03 +0700

    lecture - method chaining

---
+++ 11_method_chaining.js
@@ -0,0 +1,9 @@
+/*
+    Methid Chaining
+        เป็นการเรียกใช้ method อื่นหลังจาก method ปัจจุบันถูกเรียกใช้
+*/
+
+let username = "mint rosetta";
+
+let letter = username.charAt(0).toUpperCase();
+console.log(letter);
