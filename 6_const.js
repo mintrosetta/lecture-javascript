c26d24011adedd7733c512c3a7d5a2b87beccde1
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-31 08:27:27 +0700

    lecture - const

---
+++ 6_const.js
@@ -0,0 +1,15 @@
+/*
+    Const
+        variable ที่ไม่สามารถเปลี่ยนแปลงได้
+
+        โดยปกติจะใช้ตัวพิมใหญ่ทั้งหมดในการตั้งชื่อ
+*/
+
+const PI = 3.14159;
+let radius = 9;
+
+PI = 5; // ไม่สามารถทำได้
+
+let circumference = 2 * PI * radius;
+
+console.log(circumference);
