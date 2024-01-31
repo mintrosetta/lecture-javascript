ec8c3383e465f68ff982c1edb691fc1b8e16135f
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-01 02:12:33 +0700

    lecture - nested loop

---
+++ 20_nested_loop.js
@@ -0,0 +1,11 @@
+/*
+    Nested loop
+        เป็นการวนซ้ำที่อยู่ข้างในการซนซ้ำอีกทีหนึ่ง
+*/
+
+for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+        console.log(j)
+    }
+    console.log("-----");
+}
