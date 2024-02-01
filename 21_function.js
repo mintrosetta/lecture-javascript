2b59ba081d336fae2b38f37daaa63e350e521ac8
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-01 08:40:38 +0700

    lecture - function

--- 21_function.js
+++ 21_function.js
@@ -1,3 +1,13 @@
 /*
-    https://youtu.be/8dWL3wF_OMw?si=VlZkj2znfEjXXijP&t=5889
+    Function
+        เป็นสิ่งที่เอาไว้เก็บชุดของคำสั่ง และสามารถนำไปเรียกใช้ได้จากหลาย ๆ ที่
+        โดยไม่ต้องเขียนใหม่
 */
+
+function happyBirthday(username) {
+    if (username === undefined) console.error("Username is undefined."); 
+    console.log("Happy birthday to " + username + " <3");
+}
+
+happyBirthday("Mint Rosetta");
+happyBirthday("Bike Chanokchon");
