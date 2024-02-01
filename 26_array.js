341f962527347cd85d6f9866820857364c4482cd
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-01 18:46:12 +0700

    lecture - array

--- 26_array.js
+++ 26_array.js
@@ -1,3 +1,41 @@
 /*
-    https://youtu.be/8dWL3wF_OMw?si=4GAtfQQpKNfJhqnt&t=8150
+    array
+        เป็นตัวแปรที่สามารถเก็บได้หลายข้อมูล
 */
+
+let fruits = ["apple", "orange", "banana"];
+
+console.log(fruits);
+console.log(fruits[0])
+
+// แก้ไขข้อมูลใน index ที่ระบุ
+fruits[1] = "coconut";
+console.log(fruits[1]);
+
+// เพิ่ทข้อมูลเข้าไปใน array
+fruits.push("lemon");
+
+// ลบข้อมูลตัวสดท้าย และส่งคืนข้อมูลที่ลบ
+console.log(fruits.pop());
+
+// เพิ่มข้อมูลเข้าไปที่ด้านหน้าสุด
+fruits.unshift("mango");
+
+// ลบข้อมูลจากดด้านหน้า
+fruits.unshift();
+
+// ดูขนาดของ array
+console.log(fruits.length);
+
+// รับ index ของข้อมูลที่ค้นหา, ถ้าไม่เจอจะส่งคืน -1
+console.log(fruits.indexOf("apple"));
+
+// วนซ้ำดึงค่าใน array ด้วย index
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
+
+// ใช้ foreach ทำการดึงคต่าออกมา
+for (let fruit of fruits) {
+    console.log(fruit);
+}
