c0298bd91f1f6534b7b2f2e41cad31839b600bef
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-01 20:24:30 +0700

    lecture - rest parameter

---
+++ 30_rest_parameter.js
@@ -0,0 +1,22 @@
+/*
+    Rest Parameter
+        แสดงถึงจำนวนที่ไม่แน่นอนของ parameter 
+        ทำให้เมื่อเราใส่ arguments ลงไป มันกลายเป็น array ให้เอง
+*/
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+console.log(sum(a, b, c, d, e));
+
+function sum(...numbers) {
+    let total = 0;
+    for (let number of numbers) {
+        total += number;
+    }
+
+    return total;
+}
