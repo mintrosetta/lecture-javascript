51a4bf56c7e60988aeb0af65aeb1a0170d713900
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-01 09:09:55 +0700

    lecture - template literals

---
+++ 24_template_literals.js
@@ -0,0 +1,9 @@
+/*
+    Template literals
+        ทำให้เราสามารถแทรก ข้อมูล เข้าไประหว่าง string ได้ง่ายเข้น
+        และสามารถเว้นบรรทัดได้ง่าย
+*/
+
+let username = "Mint Rosetta";
+console.log(`Hello ${username}
+Happy Birthday <3`);
