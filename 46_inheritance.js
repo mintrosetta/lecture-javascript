6071650f4be4a02693f07d582fe135eed1f0519c
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-02 19:15:41 +0700

    lecture - inheritance

--- 46_inheritance.js
+++ 46_inheritance.js
@@ -1,3 +1,47 @@
 /*
-    https://youtu.be/8dWL3wF_OMw?si=5xuGr4eGFha-Hudo&t=12866
+    Inheritance
+        เป็นการสืบทอดคุณลักษณะและพฤติกรรมจาก super class ไปให้ sub class
 */
+
+class Animal {
+    alive = true;
+    name = "Animal";
+
+    eat() {
+        console.log(`This ${this.name} is eating`);
+    }
+
+    sleep() {
+        console.log(`This ${this.name} is sleeping`);
+    }
+}
+
+class Rabbit extends Animal {
+    name = "rabbait";
+
+    run() {
+        console.log(`This ${this.name} is running`);
+    }
+}
+
+class Fish extends Animal {
+    name = "fish";
+
+    run() {
+        console.log(`This ${this.name} is swimming`);
+    }
+}
+
+class Hawk extends Animal {
+    name = "hawk";
+
+    fly() {
+        console.log(`This ${this.name} is flying`);
+    }
+}
+
+const rabbit = new Rabbit();
+console.log(rabbit.alive);
+rabbit.eat();
+rabbit.sleep();
+rabbit.run();
