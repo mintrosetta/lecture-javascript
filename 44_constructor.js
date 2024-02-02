31c18c6228eb3987fae2a07a374744160bf15351
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-02 09:51:19 +0700

    lecture - constructor

--- 44_constructor.js
+++ 44_constructor.js
@@ -1,3 +1,20 @@
 /*
-    https://youtu.be/8dWL3wF_OMw?si=AqXKUZbVoMrjpXma&t=12437
+    Constructor
+        เป็น method ที่จะทำงานทันทีเมื่อมีการสร้าง object ของคลาส
+        โดยจะใช้ในการรับ argument เพื่อ assignment ค่าให้กับ properties 
 */
+
+class Student {
+    constructor(name, age, gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+let student = new Student("Mint Rosetta", 23, 4.00);
+console.log(student)
