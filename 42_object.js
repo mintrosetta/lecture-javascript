0dc0775c9e039cba4058fa3dbdd6c392024b73cd
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-02 08:54:57 +0700

    lecture - this

--- 42_object.js
+++ 42_object.js
@@ -3,6 +3,9 @@
         กลุ่มของ properties และ method
             properties = คุณลักษณะของ object
             method = พฤติกรรมของ object
+
+    this
+        ใช้สำหรับ reference ถึง object ตัวเอง
 */
 
 const CAR = {
@@ -10,11 +13,14 @@ const CAR = {
     color: "red",
     year: 2023,
 
-    drive: () => {
+    drive: function(){
         console.log("You drive the car");
     },
-    brake: () => {
+    brake: function() {
         console.log("You step on the brake");
+    },
+    getName: function() {
+        console.log(`Your car name is ${this.model}`);
     }
 };
 
@@ -24,3 +30,4 @@ console.log(CAR.year);
 
 CAR.drive();
 CAR.brake();
+CAR.getName();
