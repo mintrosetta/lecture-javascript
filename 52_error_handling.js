5908cf7a1db2a3a6c0f7023fd994734e6ce7cd6d
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-04 10:19:56 +0700

    lecture - error handling

--- 52_error_handling.js
+++ 52_error_handling.js
@@ -1,3 +1,21 @@
 /*
-    https://youtu.be/8dWL3wF_OMw?si=OPRx3S3IKRYcw6c5&t=14567
+    Error Handling
+        error
+            เป็น Object ที่มีรายละเอียดเกี่ยวกับข้อผิดพลาด
+            เช่น 
+                ไม่สามารถเปิดไฟล์ได้
+                ขาดการเชื่อมต่อ
+                ประเภทข้อมูลไม่ตรงกัน
 */
+
+try {
+    let id = "5a";
+
+    if (isNaN(id)) throw {message: "User id is invalid"};
+
+} catch (error) {
+    console.log(error.message);
+} finally {
+    // ทำงานหลังจาก try หรือ catch ทำงานเสร็จ
+    console.log("finally")
+}
